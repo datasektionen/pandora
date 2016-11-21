e34e887d5aabce60e7395454a50ee3bfce6cb2a8
Jonas Dahl <jonas@jdahl.se>
2016-11-21 20:50:44 +0100

    Added 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>404 - Bokningssystemet på Datasektionen</title>
+
+    <!-- Fonts -->
+    <link href="//aurora.datasektionen.se" rel="stylesheet" type="text/css">
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript">
+    window.tbaas_conf = {
+        system_name: "bokning",
+        target_id: "methone-container-replace",
+        primary_color: "#03A9F4",
+        secondary_color: "#ffffff",
+        bar_color: "#039BE5",
+                login_text: "Logga ut",
+        login_href: "/logout",
+        
+        topbar_items: [
+        { str: "Hem", href: "/" },
+                    { str: "Administrera", href: "/admin" },
+                ]
+    }
+    </script>
+    <script async src="//methone.datasektionen.se"></script>
+    </head>
+<body>
+    <div id="methone-container-replace"></div>
+    <div id="application" class="blue">
+        <header>
+            <div class="header-inner">
+                <div class="row">
+                    <div class="header-left col-md-2">
+                                            </div>
+                    <div class="col-md-8">
+                        <h2>404 - Avslaget</h2>
+                    </div>
+                    <div class="header-right col-md-2">
+                                            </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </header>
+        <div id="content">
+            <h1>Det är inte alltid lätt att vara liten</h1>
+            <p>Bokningssystemet avslog alla försök att vara hjälpsamt. Nu har du klickat på en länk som inte Jonas hade räknat med, eller skrivit något helgalet i URLen. Skapa ett issue <a href="//github.com/datasektionen/pandora/issues">här</a> om du tror att det kan hjälpa. Förlåt.</p>
+        </div>
+    </div>
+</body>
+</html>
