4c176a531459994d94415be0c310973d9ceb052e
Jonas Dahl <jonas@jdahl.se>
2016-12-08 21:51:55 +0100

    Added timezone to ical export, fixes 10

--- resources/views/ical/entity.blade.php
+++ resources/views/ical/entity.blade.php
@@ -5,9 +5,9 @@ PRODID:-//hacksw/handcal//NONSGML v1.0//EN
 @foreach($events as $event)
 BEGIN:VEVENT
 UID:{{ $event->id }}@bokning.datasektionen.se
-DTSTAMP:{{ date('Ymd\THis\Z', strtotime($event->start)) }}
-DTSTART:{{ date('Ymd\THis\Z', strtotime($event->start)) }}
-DTEND:{{ date('Ymd\THis\Z', strtotime($event->end)) }}
+DTSTAMP;TZID=Europe/Stockholm:{{ date('Ymd\THis\Z', strtotime($event->start)) }}
+DTSTART;TZID=Europe/Stockholm:{{ date('Ymd\THis\Z', strtotime($event->start)) }}
+DTEND;TZID=Europe/Stockholm:{{ date('Ymd\THis\Z', strtotime($event->end)) }}
 LOCATION:{{ $entity->name }}
 URL:{{ url('/bookings/' . $entity->id . '/' . date('Y', strtotime($event->start)) . '/' . date('W', strtotime($event->start))) }}
 SUMMARY:{{ $event->title }}
