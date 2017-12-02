9385b0fe3c83618775af309a92a54b0b12fc76e0
Jonas Dahl <jonas@jdahl.se>
2017-12-02 18:12:51 +0100

    Added status to user list

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,6 +10,7 @@
 				<th>Entitet</th>
 				<th style="width:30px;"></th>
 				<th>Beskrivning</th>
+				<th>Status</th>
 				<th>Från</th>
 				<th>Till</th>
 			</tr>
@@ -19,6 +20,7 @@
 			<td><i class="fa {{ $event->entity->fa_icon }}"></i> {{ $event->entity->name }}</td>
 			<td><a href="/bookings/{{ $event->entity_id }}/{{ $event->start->format('Y') }}/{{ $event->start->format('W') }}/{{ $event->id }}"><i class="fa fa-calendar"></i></a></td>
 			<td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+			<td>{{ $event->approved === null ? 'Ej godkänd' : 'Godkänd' }}</td>
 			<td>{{ $event->start->format('Y-m-d H:i') }}</td>
 			<td>{{ $event->end->format('Y-m-d H:i') }}</td>
 		</tr>
@@ -36,6 +38,7 @@
 				<th>Entitet</th>
 				<th style="width:30px;"></th>
 				<th>Beskrivning</th>
+				<th>Status</th>
 				<th>Från</th>
 				<th>Till</th>
 			</tr>
@@ -45,6 +48,7 @@
 			<td><i class="fa {{ $event->entity->fa_icon }}"></i> {{ $event->entity->name }}</td>
 			<td><a href="/bookings/{{ $event->entity_id }}/{{ $event->start->format('Y') }}/{{ $event->start->format('W') }}/{{ $event->id }}"><i class="fa fa-calendar"></i></a></td>
 			<td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+			<td>{{ $event->approved === null ? 'Ej godkänd' : 'Godkänd' }}</td>
 			<td>{{ $event->start->format('Y-m-d H:i') }}</td>
 			<td>{{ $event->end->format('Y-m-d H:i') }}</td>
 		</tr>
