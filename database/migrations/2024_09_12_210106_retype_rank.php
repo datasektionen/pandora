313b0bdf96f7e6241b54848a91926dbfa78c7ec6
Isac Åström <kiastrom@kth.se>
2024-09-12 21:54:41 +0200

    addera mer migrän

---
+++ database/migrations/2024_09_12_210106_retype_rank.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RetypeRank extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            $table->dropColumn('rank');
+            $table->integer('rank')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            $table->dropColumn('rank');
+        });
+    }
+}
